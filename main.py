e7769945dcd95fb7b2ba6d1f5c3161084d3064a3
Vincent <vincentt.python@gmail.com>
2025-07-13 13:50:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import time
+import threading
+from playsound import playsound
 
 counting_time = input("Gebe die Länge des Countdowns in Sekunden an:")
 counting_time = int(counting_time)
 
+def play_audio():
+    playsound('egg_clock.wav')
+
+audio_thread = threading.Thread(target=play_audio)
+
 time.sleep(counting_time-10)
 print("Zehn Sekunden verbleibend.")
+audio_thread.start()
 
 time.sleep(7)
 print("Drei Sekunden verbleibend.")
